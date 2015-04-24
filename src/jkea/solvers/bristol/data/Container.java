41db3227856984db961c83b50062048847047e34
Jonathan O'Connell <masterjonny@users.noreply.github.com>
2015-04-24 11:50:53 +0100

    Added generic tuple class

---
+++ src/jkea/solvers/bristol/data/Container.java
@@ -0,0 +1,11 @@
+package jkea.solvers.bristol.data;
+
+class Container {
+
+	private double[] scores;
+	
+	Container(double[] scores) {
+		this.scores = scores;
+	}
+	
+}
---
+++ src/jkea/solvers/bristol/data/Knapsack.java
@@ -0,0 +1,5 @@
+package jkea.solvers.bristol.data;
+
+class Knapsack {
+
+}
