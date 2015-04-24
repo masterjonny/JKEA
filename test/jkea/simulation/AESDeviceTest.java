59510f8e44b45eedff674f24a1399203f725fe95
Jonathan O'Connell <masterjonny@users.noreply.github.com>
2015-04-24 11:31:50 +0100

    Consolidate simulation engine tests

--- test/jkea/simulation/AESDeviceTest.java
+++ test/jkea/simulation/AESDeviceTest.java
@@ -1,6 +1,7 @@
 package jkea.simulation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 
@@ -13,58 +14,53 @@ public class AESDeviceTest {
 
 	@Before
 	public void setUp() throws Exception {
-		ArrayList<HammingTemplate> testTemplates = new ArrayList<HammingTemplate>(
+		final ArrayList<HammingTemplate> testTemplates = new ArrayList<HammingTemplate>(
 				16);
-		for (int i = 0; i < 16; i++) {
+		for (int i = 0; i < 16; i++)
 			testTemplates.add(new HammingTemplate(1));
-		}
 		testClass = new AESDevice(testTemplates);
+	}
+
+	@Test
+	public void testGetTrace() {
+		final double[] trace = testClass.getTrace();
+		assertEquals(0x10, trace.length);
+
 	}
 
 	@Test
 	public void testNewKey() {
 		testClass.newKey();
-		short[] oldKey = testClass.getKey();
+		final short[] oldKey = testClass.getKey();
 		testClass.newKey();
-		short[] newKey = testClass.getKey();
+		final short[] newKey = testClass.getKey();
 
-		int shortestLength = Math.min(oldKey.length, newKey.length);
+		final int shortestLength = Math.min(oldKey.length, newKey.length);
 
 		int matchingCount = 0;
-		for (int i = 0; i < shortestLength; i++) {
+		for (int i = 0; i < shortestLength; i++)
 			if (oldKey[i] == newKey[i])
 				matchingCount++;
-		}
 		if ((matchingCount == shortestLength)
-				&& (oldKey.length == newKey.length)) {
+				&& (oldKey.length == newKey.length))
 			fail("Key unchanged");
-		}
 	}
 
 	@Test
 	public void testNewPlain() {
 		testClass.newPlain();
-		short[] oldPlain = testClass.getPlain();
+		final short[] oldPlain = testClass.getPlain();
 		testClass.newPlain();
-		short[] newPlain = testClass.getPlain();
+		final short[] newPlain = testClass.getPlain();
 
-		int shortestLength = Math.min(oldPlain.length, newPlain.length);
+		final int shortestLength = Math.min(oldPlain.length, newPlain.length);
 
 		int matchingCount = 0;
-		for (int i = 0; i < shortestLength; i++) {
+		for (int i = 0; i < shortestLength; i++)
 			if (oldPlain[i] == newPlain[i])
 				matchingCount++;
-		}
 		if ((matchingCount == shortestLength)
-				&& (oldPlain.length == newPlain.length)) {
+				&& (oldPlain.length == newPlain.length))
 			fail("Plain unchanged");
-		}
-	}
-
-	@Test
-	public void testGetTrace() {
-		double[] trace = testClass.getTrace();
-		assertEquals(0x10, trace.length);
-
 	}
 }
--- test/jkea/simulation/HammingTemplateTest.java
+++ test/jkea/simulation/HammingTemplateTest.java
@@ -1,7 +1,6 @@
 package jkea.simulation;
 
-import static org.junit.Assert.*;
-import jkea.simulation.HammingTemplate;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,19 +9,14 @@ public class HammingTemplateTest {
 
 	HammingTemplate testClass;
 
-	@Before
-	public void setUp() throws Exception {
-		testClass = new HammingTemplate(1.0);
-	}
-
 	@Test
 	public void adjustVarianceTest() {
-		double stdDevSolutions[] = { 1, 1.41421, 1.73205, 2, 2.23607, 2.44949,
-				2.64575, 2.82843, 3 };
-		double cNormSolutions[] = { 0.398942, 0.282095, 0.230329, 0.199471,
-				0.178412, 0.162868, 0.150786, 0.141047, 0.132981 };
+		final double stdDevSolutions[] = { 1, 1.41421, 1.73205, 2, 2.23607,
+				2.44949, 2.64575, 2.82843, 3 };
+		final double cNormSolutions[] = { 0.398942, 0.282095, 0.230329,
+				0.199471, 0.178412, 0.162868, 0.150786, 0.141047, 0.132981 };
 		for (int i = 1; i < 10; i++) {
-			HammingTemplate newClass = new HammingTemplate(i);
+			final HammingTemplate newClass = new HammingTemplate(i);
 			assertEquals(stdDevSolutions[i - 1], newClass.getStdDeviation(),
 					1e-5);
 			assertEquals(cNormSolutions[i - 1], newClass.getNorm(), 1e-5);
@@ -31,45 +25,50 @@ public class HammingTemplateTest {
 
 	@Test
 	public void hammingWeightTest() {
-		short solutions[] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
-				1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2,
-				3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4,
-				4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4,
-				5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4,
-				3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4,
-				5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4,
-				4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3,
-				4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6,
-				4, 5, 5, 6, 5, 6, 6, 7, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4,
-				5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 3, 4,
-				4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6,
-				7, 5, 6, 6, 7, 6, 7, 7, 8 };
-		for (short i = 0; i < 256; i++) {
+		final short solutions[] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3,
+				3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2,
+				3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5,
+				3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3,
+				4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3,
+				3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5,
+				6, 4, 5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4,
+				3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2,
+				3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5,
+				5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4,
+				5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+				3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5,
+				6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8 };
+		for (short i = 0; i < 256; i++)
 			assertEquals(solutions[i], testClass.hammingWeight(i));
-		}
 	}
 
 	public void leakDensityTest() {
-		double solutions[] = { 0.282095, 0.0297326, 0.0297326, 0.00516675,
-				0.0297326, 3.48133e-05, 0.277721, 0.0425899, 0.0425899,
-				0.00838646, 0.0425899, 7.25571e-05, 0.265004, 0.0591303,
-				0.0591303, 0.0131937, 0.0591303, 0.000146569, 0.245088,
-				0.0795685, 0.0795685, 0.0201181, 0.0795685, 0.000286969,
-				0.219696, 0.103777, 0.103777, 0.0297326, 0.103777, 0.000544571 };
+		final double solutions[] = { 0.282095, 0.0297326, 0.0297326,
+				0.00516675, 0.0297326, 3.48133e-05, 0.277721, 0.0425899,
+				0.0425899, 0.00838646, 0.0425899, 7.25571e-05, 0.265004,
+				0.0591303, 0.0591303, 0.0131937, 0.0591303, 0.000146569,
+				0.245088, 0.0795685, 0.0795685, 0.0201181, 0.0795685,
+				0.000286969, 0.219696, 0.103777, 0.103777, 0.0297326, 0.103777,
+				0.000544571 };
 		int count = 0;
-		for (double i = 0; i <= 1; i += 0.25) {
-			for(short j = 0; j < 256; j += 25) {
-				count ++;
-				assertEquals(solutions[count], testClass.leakDensity(i, j), 1e-5);
+		for (double i = 0; i <= 1; i += 0.25)
+			for (short j = 0; j < 256; j += 25) {
+				count++;
+				assertEquals(solutions[count], testClass.leakDensity(i, j),
+						1e-5);
 			}
-		}
 	}
-	
+
 	public void leakTest() {
-		double solutions[] = { 2.41421, 4.41421, 7.41421, 5.41421,  6.41421 }; 
-		short inputs[] = { 1, 100, 125, 57, 227 };
-		for(int i = 0; i < 5; i++) {
+		final double solutions[] = { 2.41421, 4.41421, 7.41421, 5.41421,
+				6.41421 };
+		final short inputs[] = { 1, 100, 125, 57, 227 };
+		for (int i = 0; i < 5; i++)
 			assertEquals(solutions[i], testClass.leak(inputs[i]), 1 + 1e-5);
-		}
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		testClass = new HammingTemplate(1.0);
 	}
 }
